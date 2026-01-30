79225d0a16f254c63c1fb118ad6980d8b7ceed6f
xbrovv <ishanxbro847@gmail.com>
2026-01-30 15:12:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 module.exports = {
 
   // 🔐 WhatsApp Session ID (MEGA or base64)
-  SESSION_ID: process.env.SESSION_ID || "79AHXBpI#V9eFGntqOLKy2hK3iWE7dD7sVtMed4FwlFUrc3wZHhg",
+  SESSION_ID: process.env.SESSION_ID || "78RnjDSK#fwgewvE5wHyFUzUmqQs5XLmoL-cQbQ-yHgoUPgIzujI",
 
 
   // ===============================
