8be776039b6807a05852c76d9ce6a193f6b1f694
xbrovv <ishanxbro847@gmail.com>
2026-01-31 15:58:26 +0530

    Refactor menu command and improve response handling

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,217 +1,136 @@
-const {readEnv} = require('../config')
-const {cmd , commands} = require('../command')
-const os = require("os")
-const {runtime} = require('../lib/functions')
+// plugins/menu.js
+const { cmd, commands } = require("../command");
+const config = require("../config");
+
 cmd({
     pattern: "menu",
-    alias: ["list"],
-    desc: "bot's commands",
     react: "📜",
-    category: "main"
+    alias: ["panel", "commands"],
+    desc: "Get Bot Menu",
+    category: "main",
+    use: '.menu',
+    filename: __filename
 },
-async (ishan, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+async (ishan, mek, m, { from, quoted, pushname, reply }) => {
     try {
-        let desc = `*👋 Hello ${pushname}*
+        const config = await readEnv();
 
-*╭─「 ${config.BOT_NAME} 」*
-*│◈ ʀᴜɴᴛɪᴍᴇ : ${runtime(process.uptime())}*
-*│◈ ʀᴀᴍ ᴜꜱᴀɢᴇ : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
-*│◈ ᴘʟᴀᴛꜰᴏʀᴍ : ${os.hostname()}*
-*│◈ ᴠᴇʀꜱɪᴏɴ : 3.0.0*
+        const qMessage = {
+  key: {
+    fromMe: false,
+    remoteJid: "status@broadcast",
+    participant: "0@s.whatsapp.net",
+  },
+  message: {
+    locationMessage: {
+      degreesLatitude: 40.7128, 
+      degreesLongitude: -74.0060, 
+      name: "Monaragala",  
+      address: "Siyambalanduwa", 
+    }
+  }
+};
+
+        
+        const date = moment().tz("Asia/Colombo").format("YYYY-MM-DD");
+        const time = moment().tz("Asia/Colombo").format("HH:mm:ss");
+
+        let host = os.hostname() || "render";
+        if (host.length === 12) host = 'replit';
+        else if (host.length === 36) host = 'heroku';
+        else if (host.length === 8) host = 'koyeb';
+        
+        const ownerdata = (await axios.get('https://raw.githubusercontent.com/ishanxmd/Data/refs/heads/main/details.json')).data;
+        const OWNER_NUMBER = ownerdata.ownernumber;
+        const FOOTER = ownerdata.footer;
+        const IMAGE_URL = ownerdata.imageurl;
+        const ALIVE_VIDEO = ownerdata.alivevideo;
+        const OWNER_NAME = ownerdata.ownername;
+        
+        const selectionMessage = `👋 *Hello, ${pushname}*
+*🫟 Wᴇʟᴄᴏᴍᴇ Tᴏ Qᴜᴇᴇɴ-ɴᴇᴛʜᴜ-Mᴅ*🫟*
+        
+*╭─「 ꜱᴛᴀᴛᴜꜱ ᴅᴇᴛᴀɪʟꜱ 」*
+*│* 🤵 *\`Owner\`* : ${OWNER_NAME}
+*│* 📞 *\`Owner Number\`* : ${OWNER_NUMBER}
+*│* 🚀 *\`Prefix\`* : ${config.PREFIX}
+*│* 🕒 *\`Time\`* : ${time}
+*│* 📅 *\`Date\`* : ${date}
+*│* 📋 *\`Categories\`* : 9
 *╰──────────●●►*
 
-*╭╼╼╼╼╼╼╼╼╼╼*
-*├ 1 • MAIN*
-*├ 2 • SEARCH*
-*├ 3 • DOWNLOAD*
-*├ 4 • GROUP*
-*├ 5 • OWNER*
-*├ 6 • FUN*
-*╰╼╼╼╼╼╼╼╼╼╼*
 
-_*🌟 Reply with the Number you want to select*_
+*\`Reply Below Number 🔢\`*
 
-> *𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 your Botname*`;
+│ ◦ *1* \`\`\`OWNER MENU\`\`\`
+│ ◦ *2* \`\`\`AI MENU\`\`\`
+│ ◦ *3* \`\`\`SEARCH MENU\`\`\`
+│ ◦ *4* \`\`\`DOWNLOAD MENU\`\`\`
+│ ◦ *5* \`\`\`MAIN MENU\`\`\`
+│ ◦ *6* \`\`\`CONVERT MENU\`\`\`
+│ ◦ *7* \`\`\`OTHER MENU\`\`\`
+│ ◦ *8* \`\`\`LOGO MENU\`\`\`
+│ ◦ *9* \`\`\`GROUP MENU\`\`\`
 
-        const vv = await ishan.sendMessage(from, { image: { url: config.MENU_IMG}, caption: desc }, { quoted: mek });
+${FOOTER}`;
+
+        await ishan.sendMessage(from, {
+            video: { url: ALIVE_VIDEO },
+            mimetype: 'video/mp4',
+            ptv: true
+        }, { quoted: mek });
+
+        const sentMsg = await conn.sendMessage(from, {
+            image: { url: IMAGE_URL },
+            caption: selectionMessage,
+            contextInfo: { forwardingScore: 999, isForwarded: false }
+        }, { quoted: mek });
 
         ishan.ev.on('messages.upsert', async (msgUpdate) => {
             const msg = msgUpdate.messages[0];
             if (!msg.message || !msg.message.extendedTextMessage) return;
 
-            const selectedOption = msg.message.extendedTextMessage.text.trim();
+            const userResponse = msg.message.extendedTextMessage.text.trim();
+            if (msg.message.extendedTextMessage.contextInfo &&
+                msg.message.extendedTextMessage.contextInfo.stanzaId === sentMsg.key.id) {
 
-            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
-                switch (selectedOption) {
-                    case '1':
-                    reply(`
-                    
+                const menuCategories = {
+                    '1': 'owner',
+                    '2': 'ai',
+                    '3': 'search',
+                    '4': 'download',
+                    '5': 'main',
+                    '6': 'convert',
+                    '7': 'other',
+                    '8': 'auto',
+                    '9': 'group'
+                };
 
-╔════════════════════════╗  
-║ 🔧 **𝗠𝗔𝗜𝗡 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧** 🔧 ║  
-╚════════════════════════╝  
-
-╭─━─〔 ⚡ **Commands** ⚡ 〕━━╮  
-┃ ◈ **alive**
-┃ ◈ **menu**  
-┃ ◈ **menu2**  
-┃ ◈ **system**  
-┃ ◈ **ping**  
-┃ ◈ **runtime**
-┃ ◈ **jid**
-╰─━─━─━─━─━─━─━─━─╯  
-
-📊 **Total Commands in MAIN:** 7  
- 
-> 💡 **𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 Your Botname**  
-
-`);
-
-                        break;
-                    case '2':               
-                        reply(`
-
-╔════════════════════════╗  
-║ 🔍 **𝗦𝗘𝗔𝗥𝗖𝗛 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧** 🔍 ║  
-╚════════════════════════╝  
-
-╭─━〔 ⚡ **Commands** ⚡ 〕━──━╮  
-┃ ◈ **yts**  
-┃ ◈ **image** 
-╰─━─━─━━─━─━─━─━─━─╯  
-
-📊 **Total Commands in SEARCH:** 2
-
-> 💡 **𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 your Botname**
-`);
-                        break;
-                    case '3':               
-                        reply(`
-╔════════════════════════╗  
-║ 📥 **𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧** 📥 ║  
-╚════════════════════════╝  
-
-╭─━━〔 ⚡ **Commands** ⚡ 〕━─━━╮  
-┃ ◈ **apk**  
-┃ ◈ **twitter**  
-┃ ◈ **gdrive**  
-┃ ◈ **mediafire**  
-┃ ◈ **fb**  
-┃ ◈ **play**
-┃ ◈ **play2**  
-┃ ◈ **video**   
-┃ ◈ **video2**  
-┃ ◈ **yta**  
-┃ ◈ **tiktok**
-┃ ◈ **ytmp3**
-╰─━─━─━─━─━─━─━─━─━─╯  
-
-📊 **Total Commands in DOWNLOAD:** 12
- 
-> 💡 **𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 your Botname**  
-
-`);
-                    
-                        break;
-                    case '4':               
-                        reply(`
-╔════════════════════════╗  
-║ 👥 **𝗚𝗥𝗢𝗨𝗣 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧** 👥 ║  
-╚════════════════════════╝  
-
-╭─━──━〔 ⚡ **Commands** ⚡ 〕━─━╮  
-┃ ◈ **mute**  
-┃ ◈ **unmute**  
-┃ ◈ **promote**  
-┃ ◈ **demote**  
-┃ ◈ **del**  
-┃ ◈ **add**  
-┃ ◈ **admins**  
-┃ ◈ **groupdesc**  
-┃ ◈ **groupinfo**  
-┃ ◈ **gname**  
-┃ ◈ **setsubject**  
-┃ ◈ **tagall**  
-┃ ◈ **hidetag**  
-┃ ◈ **unlock**  
-┃ ◈ **lock**
-┃ ◈ **gname**  
-┃ ◈ **join**  
-┃ ◈ **leave**  
-┃ ◈ **invite**  
-┃ ◈ **tagadmin**  
-╰─━─━─━─━─━─━─━─━─━━─╯  
-
-📊 **Total Commands in GROUP:** 20  
-
-
-> 💡 **𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 Your Botname**  
-`);
-                    break;
-                    case '5':               
-                        reply(`
-╔════════════════════════╗  
-║ 👨‍💻 **𝗢𝗪𝗡𝗘𝗥 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧** 👨‍💻 ║  
-╚════════════════════════╝  
-
-╭─━〔 🍿 **Commands** 🍿 〕━──━╮ 
-┃ ◈ **shutdown**  
-┃ ◈ **alive**  
-┃ ◈ **ping**  
-┃ ◈ **clearchats**  
-┃ ◈ **block**
-┃ ◈ **unblock**
-┃ ◈ **repo**
-┃ ◈ **owner**
-┃ ◈ **owner2**
-╰─━━─━─━──━─━─━━─━─╯  
-
-📊 **Total Commands in Owner:** 9
-
- 
-> 💡 **𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 your Botname**  
-
-`);
-                    break;
-                    case '6':               
-                        reply(`
-╔════════════════════════╗  
-║ 👨‍💻 **𝐓𝐎𝐎𝐋𝐒 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧** 👨‍💻 ║  
-╚════════════════════════╝  
-
-╭─━〔 🍿 **Commands** 🍿 〕━──━╮ 
-┃ ◈ **joke**  
-┃ ◈ **flirt**  
-┃ ◈ **truth**  
-┃ ◈ **dare**  
-┃ ◈ **fact**
-┃ ◈ **pickupline**
-┃ ◈ **character**
-┃ ◈ **repeat**
-┃ ◈ **spam**
-┃ ◈ **readmore**
-╰─━━─━─━──━─━─━━─━─╯  
-
-📊 **Total Commands in Owner:** 10
-
- 
-> 💡 **𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 your Botname**  
-
-`);
-                       
-                        
-                    break;
-                    default:
-                    
-                        reply("Invalid option. Please select a valid option🔴");
+                if (!menuCategories[userResponse]) {
+                    await reply("*Please Reply The Number ❗❗*");
+                    return;
                 }
 
+                const selectedCategory = menuCategories[userResponse];
+                let menu = '';
+
+                for (let i = 0; i < commands.length; i++) {
+                    if (commands[i].category === selectedCategory && !commands[i].dontAddCommandList) {
+                        menu += `*╭──────────●●►*\n*│Command:* ${commands[i].pattern}\n*│Desc:* ${commands[i].desc}\n*│Use:* ${commands[i].use}\n*╰──────────●●►*\n\n`;
+                    }
+                }
+
+                const madeMenu = `*◈ ${selectedCategory.charAt(0).toUpperCase() + selectedCategory.slice(1)} Command List ◈*\n\n${menu}─────────────────────────\n${FOOTER}`;
+
+                await ishan.sendMessage(from, {
+                    image: { url: IMAGE_URL },
+                    caption: madeMenu
+                }, { quoted: qMessage });
             }
         });
 
-    } catch (e) {
-        console.error(e);
-        await ishan.sendMessage(from, { react: { text: '❌', key: mek.key } })
-        reply('An error occurred while processing your request.');
+    } catch (err) {
+        console.error(err);
+        await reply('*ERROR ❗❗*');
     }
-});
+}); 
